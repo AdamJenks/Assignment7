02059986892d2e33b85404e88f7fb481559bc7c3
AdamJenks <adam.jenks@email.wsu.edu>
2017-11-20 20:40:34 -0800

    Multiview update

--- SvensonLog/LogProcessApp.aspx.vb
+++ SvensonLog/LogProcessApp.aspx.vb
@@ -8,4 +8,18 @@ Partial Class LogProcessApp
     Protected Sub Page_Load(sender As Object, e As EventArgs) Handles Me.Load
 
     End Sub
+
+    Protected Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles chkVip.CheckedChanged
+
+    End Sub
+
+    Protected Sub LinkButton1_Click(sender As Object, e As EventArgs) Handles LinkButton1.Click
+        MultiView1.ActiveViewIndex = 0 'Click button see Sawyers INSERT page
+    End Sub
+    Protected Sub LinkButton2_Click(sender As Object, e As EventArgs) Handles LinkButton2.Click
+        MultiView1.ActiveViewIndex = 1 'Click button see Trucker INSERT page
+    End Sub
+    Protected Sub LinkButton3_Click(sender As Object, e As EventArgs) Handles LinkButton3.Click
+        MultiView1.ActiveViewIndex = 2 'Click button for Delivery INSERT page.
+    End Sub
 End Class
