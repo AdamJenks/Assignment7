c5b3f44185f74cb040ba16dfc56ad0aaf1ac0d84
AdamJenks <adam.jenks@email.wsu.edu>
2017-11-20 19:16:57 -0800

    Add files via upload

---
+++ SvensonLog/LogProcessApp.aspx.vb
@@ -0,0 +1,11 @@
+﻿
+Partial Class LogProcessApp
+    Inherits System.Web.UI.Page
+
+    Protected Sub MultiView1_ActiveViewChanged(sender As Object, e As EventArgs) Handles MultiView1.ActiveViewChanged
+
+    End Sub
+    Protected Sub Page_Load(sender As Object, e As EventArgs) Handles Me.Load
+
+    End Sub
+End Class
